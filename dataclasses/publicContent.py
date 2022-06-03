cab905f14f2c36b91775ceb5b7e483cd1c4047db
Kim Roger <kim.roger@krogerdev.no>
2022-06-03 10:58:07 +0200

    Removed outdated projectfiles from outdated tutorial

---
+++ dataclasses/publicContent.py
@@ -0,0 +1,1 @@
+class Public_Content(Resource):
--- run.py
+++ run.py
@@ -1,2 +1,11 @@
+# TODO: Tutorial I'm following: https://morioh.com/p/1d2afc550822
+# Required imports
+import os
+
+
+
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
+
 if __name__ == '__main__':
-    print('Hello World!')
+    app.run()
